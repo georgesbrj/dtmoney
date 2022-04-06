cbba89960697d20750c27dc2c3cc621c19806233
George Bezerra <george.bezerra@fgv.br>
2022-04-06 18:48:51 -0300

    add folhas de estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { GlobalStyled } from "./styles/global";
+
  
 export function App() {
   return (
     <div className="App">
+       <GlobalStyled/>
        <h1>Ola mundo</h1>
+       
     </div>
   );
 }
